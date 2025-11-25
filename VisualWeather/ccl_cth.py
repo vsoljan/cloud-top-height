cb477ed7f097d8d67c08658697abfe9690854233
Vinko Soljan <vsoljan@gmail.com>
2025-11-25 13:18:36 +0100

    Revise usage instructions in ccl_cth.py
    Updated usage notes for the ccl_cth.py script.

--- VisualWeather/ccl_cth.py
+++ VisualWeather/ccl_cth.py
@@ -60,7 +60,7 @@ VERSION:
 NOTES (HOW TO USE):
 1. Copy script to (every workstation!):
 $METPATH/python/Custom/HR_CROCONTROL/Plugins/Kernel/
-(replace HR_CROCONTORL with your organisation name)
+(replace HR_CROCONTROL with your organisation name)
 IMPORTANT THING IS TO HAVE "__init__.py" file in every subdirectory!!!
 
 2. Execute in shell:
@@ -69,6 +69,10 @@ kutil f | grep ccl
 
 3. Test custom function:
 equationd 'v350[2,1] v-65[2,0] Fccl_cth_theta_e_python$v21v20$v37'
+
+You should get response like this:
+<?xml version="1.0" encoding="UTF-8"?>
+<value value="449" unit-class="3" unit-sub="7"/>
 """
 
 import numpy as np
